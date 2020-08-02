9b99db819804c3ba421731b04cb3e2652f64e0dc
Chenming Shi <clevercshi@gmail.com>
2020-08-02 20:07:17 +0800

    added tag_repository

--- lib/core/db.dart
+++ lib/core/db.dart
@@ -1,35 +1,22 @@
-import 'package:path_provider/path_provider.dart';
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 import 'dart:async';
-import 'dart:io';
 import 'package:timato/core/event.dart';
 import 'dart:developer' as developer;
 
-import 'package:timato/core/tag.dart';
+import 'package:timato/core/tag_repository.dart';
 
-abstract class DatabaseHelper {
-  // static DatabaseHelper _databaseHelper; // Singleton DatabaseHelper
+class DatabaseHelper {
   static Database _database; // Singleton Database
 
-  // DatabaseHelper._createInstance(); // Named constructor to create instance of DatabaseHelper
-
-  // factory DatabaseHelper() {
-  //   if (_databaseHelper == null) {
-  //     _databaseHelper = DatabaseHelper
-  //         ._createInstance(); // This is executed only once, singleton object
-  //   }
-  //   return _databaseHelper;
-  // }
-
-  Future<Database> get database async {
+  static Future<Database> get database async {
     if (_database == null) {
-      _database = await initializeDatabase();
+      _database = await _initializeDatabase();
     }
     return _database;
   }
 
-  Future<Database> initializeDatabase() async {
+  static Future<Database> _initializeDatabase() async {
     // Get the directory path for both Android and iOS to store database.
     // Directory directory = await getApplicationDocumentsDirectory();
     // String path = directory.path + '/core/timato.db';
@@ -43,20 +30,13 @@ abstract class DatabaseHelper {
     return database;
   }
 
-  void _createDb(Database db, int newVersion) async {
+  static void _createDb(Database db, int newVersion) async {
     EventEntity.createDb(db, newVersion);
     TagEntity.createDb(db, newVersion);
-    // 	await db.execute('CREATE TABLE $eventTable($colId INTEGER PRIMARY KEY AUTOINCREMENT, $colKey TEXT, '
-    // + '$colTaskName TEXT, $colTag TEXT, $colPriority INTEGER, $colDDL TEXT, $colDuration INTEGER), $colUnplanned INTEGER, $colToday INTEGER');
   }
 
-  void _upgradeDb(Database db, int oldVersion, int newVersion) async {
+  static void _upgradeDb(Database db, int oldVersion, int newVersion) async {
     EventEntity.upgradeDb(db, oldVersion, newVersion);
     TagEntity.upgradeDb(db, oldVersion, newVersion);
   }
-
-  // void _dropDb(Database db) async {
-  //   EventEntity.dropDb(db);
-  //   // await db.execute('DROP TABLE $eventTable');
-  // }
 }
--- lib/core/tag_repository.dart
+++ lib/core/tag_repository.dart
@@ -1,14 +1,94 @@
-import 'db.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:timato/core/db.dart';
+import 'package:timato/ui/add_event.dart';
 
-class TagRepository extends DatabaseHelper {
-  static TagRepository _databaseHelper; // Singleton DatabaseHelper
-  TagRepository._createInstance(); // Named constructor to create instance of DatabaseHelper
+class TagEntity {
+  static final String tagTable = 'tag_table';
+  static final String colDate = 'date';
+  static final String colTag = 'tag';
+  static final String colNum = 'num';
 
-  factory TagRepository() {
-    if (_databaseHelper == null) {
-      _databaseHelper = TagRepository
-          ._createInstance(); // This is executed only once, singleton object
-    }
-    return _databaseHelper;
+  static void createDb(Database db, int newVersion) async {
+    await db.execute(
+        'CREATE TABLE $tagTable($colDate TEXT, $colTag TEXT, $colNum INTEGER)');
   }
+
+  static upgradeDb(Database db, int oldVersion, int newVersion) async {
+    if (newVersion == 3) {
+      await db.execute(
+          'CREATE TABLE $tagTable($colDate TEXT, $colTag TEXT, $colNum INTEGER)');
+    }
+  }
+}
+
+void updateTag(String tag) async {
+  Database db = await DatabaseHelper.database;
+  var tags;
+  tags = await db.query(TagEntity.tagTable,
+      where:
+          "${TagEntity.colDate} = ${dateOnly(DateTime.now()).toString()} and ${TagEntity.colTag} = ${tag ?? 'is null'}");
+
+  if (tags.length == 0) {
+    await db.insert(TagEntity.tagTable, {
+      TagEntity.colDate: dateOnly(DateTime.now()).toString(),
+      TagEntity.colTag: tag,
+      TagEntity.colNum: 1
+    });
+  } else {
+    var tagRow = tags[0];
+    await db.update(TagEntity.tagTable, {TagEntity.colNum: tagRow[TagEntity.colNum]},
+        where:
+            "${TagEntity.colDate} = ${tagRow[TagEntity.colDate]} and ${TagEntity.colTag} = ${tag ?? 'is null'}");
+  }
+}
+
+Future<List<int>> getWeekTimerNum() async {
+  Database db = await DatabaseHelper.database;
+  DateTime today = dateOnly(DateTime.now());
+  List<int> result = [];
+  for (int i = 0; i < 7; ++i) {
+    List<Map<String, dynamic>> tags = await db.query(TagEntity.tagTable,
+        columns: [TagEntity.colNum],
+        where: "${TagEntity.colDate} = ${today.toString()}");
+    int sum = tags.fold(0, (previousValue, element) => previousValue + element[TagEntity.colNum]);
+    result.insert(0, sum);
+  }
+  return result;
+}
+
+Future<Map<String, int>> getTodayTagTimerNum() async{
+  Database db = await DatabaseHelper.database;
+  DateTime today = dateOnly(DateTime.now());
+  Map<String,int> result = {};
+  List<Map<String, dynamic>> tags = await db.query(TagEntity.tagTable,
+      columns: [TagEntity.colNum, TagEntity.colTag],
+      where: "${TagEntity.colDate} = ${today.toString()}");
+  for (var tagRow in tags){
+    result.addAll({tagRow[TagEntity.colTag] : tagRow[TagEntity.colNum]});
+  }
+  return result;
+}
+
+Future<Map<String, int>> getWeekTagTimerNum() async{
+  Database db = await DatabaseHelper.database;
+  List<Map<String, dynamic>> tags;
+  DateTime today = dateOnly(DateTime.now());
+  Map<String,int> result = {};
+  for (int i = 0; i < 7; ++i) {
+    tags.addAll(await db.query(TagEntity.tagTable,
+        columns: [TagEntity.colNum],
+        where: "${TagEntity.colDate} = ${today.toString()}"));
+  }
+  for (var tagRow in tags){
+    String key = tagRow[TagEntity.colTag];
+    if (result.containsKey(key)){
+      result.update(key, (value) => value + tagRow[TagEntity.colNum]);
+    }
+  }
+  return result;
+}
+
+printDatabase() async{
+  Database db = await DatabaseHelper.database;
+  print(await db.query(TagEntity.tagTable));
 }
